7a376c13870c7b40ab61b853e72314b1dbfdb955
iceyyspicyy <aryen.mhrzn@outlook.com>
2024-06-10 18:57:19 +0545

    day 1 - added models and html template from bootstrap, no views initiazed

---
+++ store/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+import datetime
+
+# Categories of Product
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+
+#customers
+class Customer(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+
+#products model
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name
+
+#customer orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    date = models.DateField(default= datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
