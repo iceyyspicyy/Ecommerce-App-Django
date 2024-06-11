84a977fb3de7b3193f84c76cf7f40bca0ed75f24
iceyyspicyy <aryen.mhrzn@outlook.com>
2024-06-11 14:20:48 +0545

    added login, signup and sales for product

--- store/models.py
+++ store/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "categories"
+
 
 #customers
 class Customer(models.Model):
@@ -29,6 +32,10 @@ class Product(models.Model):
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
+    #to determine sales stuff
+
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     def __str__(self):
         return self.name
 
--- store/views.py
+++ store/views.py
@@ -1,5 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import authenticate, login, logout
+
+#for user registration the below 3 imports
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
+
+#gives messages if its wrong when logging in
+from django.contrib import messages
+
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products':products})
+
+def about(request):
+    return render(request, 'about.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully entered the verse!!")
+            return redirect('home')
+        else:
+            messages.success(request, "Cant verify you! Are you a human or what? ")
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out! Adios....")
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #login the user
+            user = authenticate(username=username, password = password)
+            login(request, user)
+            messages.success(request, "Registered successfully..")
+            return redirect('home')
+        else:
+            messages.success(request, "Oops. Account registration failed")
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
