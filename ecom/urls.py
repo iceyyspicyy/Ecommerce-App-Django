8556693d7013fa5a5c62ae7268d0575c7145fc70
iceyyspicyy <aryen.mhrzn@outlook.com>
2024-06-14 13:01:52 +0545

    added checkout page

--- ecom/urls.py
+++ ecom/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
+    
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 # Create your views here.
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
+
+def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+
+    if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        #checkout as login
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": shipping_form})
+    
+    else:
+        #checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form":shipping_form})
