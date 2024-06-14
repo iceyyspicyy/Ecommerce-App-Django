21af049176586dfee2ac8ed814851ab7864da3e5
iceyyspicyy <aryen.mhrzn@outlook.com>
2024-06-14 10:41:36 +0545

    added stuffs for payment/shipping

--- ecom/urls.py
+++ ecom/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('payment/', include('payment.urls')),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
