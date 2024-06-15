7ea0c6b150b2cd7d00e8fab4765a7fac3cb25af5
iceyyspicyy <aryen.mhrzn@outlook.com>
2024-06-15 12:25:00 +0545

    added shipping and made inline modelling stuffs

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
-# Register your models here.
+from django.contrib.auth.models import User
 
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+#inline puts something from one model together with the next
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+#extend order model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_ordered"]
+    fields = ['user', 'full_name', 'email', 'shipping_address', 'amount_paid', 'date_ordered', 'shipped']
+    inlines = [OrderItemInline]
+
+
+#unregister order model and reregister
+admin.site.unregister(Order)
+admin.site.register(Order, OrderAdmin)
--- payment/views.py
+++ payment/views.py
@@ -4,6 +4,7 @@ from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
+from store.models import Product
 # Create your views here.
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
@@ -34,11 +35,65 @@ def process_order(request):
             #create order
             create_order = Order(user=user, full_name = full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
+
+            #add order items
+            #get order ID
+            order_id = create_order.pk
+            #get product id
+            for product in cart_products():
+                #get prodcut id
+                product_id = product.id
+                #get sales price of prodct
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                #get quantiyy
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        #create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value,price=price)
+                        create_order_item.save()
+
+            #delete our cart 
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    #delete key
+                    del request.session[key]
+
+
+
             messages.success(request,"Order Placed! Jingle Bells!")
             return redirect('home')
         else:
             create_order = Order(full_name = full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
+            order_id = create_order.pk
+            #get product id
+            for product in cart_products():
+                #get prodcut id
+                product_id = product.id
+                #get sales price of prodct
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                #get quantiyy
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        #create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value,price=price)
+                        create_order_item.save()
+
+            #delete our cart 
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    #delete key
+                    del request.session[key]
+
+
             messages.success(request,"Order Placed! Jingle Bells!")
             return redirect('home')
 
